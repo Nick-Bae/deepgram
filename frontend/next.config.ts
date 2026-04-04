4a43a1d691c6f539483d7b764a7d697affc57872
namjubae <namjubae@gmail.com>
2026-04-03 23:00:37 -0500

    google sigin fix

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,9 +1,16 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
   reactStrictMode: true,
   devIndicators: false,
+  async rewrites() {
+    return [
+      {
+        source: "/__/auth/:path*",
+        destination: "https://sturdy-dogfish-472313-k6.firebaseapp.com/__/auth/:path*",
+      },
+    ];
+  },
 };
 
 export default nextConfig;
